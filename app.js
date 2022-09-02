14464300b1799043001dc7087d2deee224ebaa1e
bayajidalam <bayajidalam2001@gmail.com>
2022-09-03 00:53:28 +0600

    header added

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('okkk')
+const loadCaregory = () =>{
+  fetch(`https://openapi.programming-hero.com/api/news/categories`)
+  .then(res =>res.json())
+  .then(data => displayCategory(data.data.news_category[0].category_name));
+}
+
+
+const displayCategory= data =>{
+  const categoryContainer = document.getElementById('category-container');
+  for(const category of data){
+    console.log(category)
+  }
+
+}
+displayCategory()
